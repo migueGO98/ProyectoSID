7e63457823b10cd91bc7b3d9bd5812e8f96b125d
Miguel Guzman <miguel.gm98@gmail.com>
2023-10-17 21:08:31 -0600

    primer prueba de api rest

--- backend/src/main/java/mx/agr/dgec/controllers/Ejemplo.java
+++ backend/src/main/java/mx/agr/dgec/controllers/Ejemplo.java
@@ -1,4 +1,16 @@
 package mx.agr.dgec.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Ejemplo {
+
+    private static final String TEMPLATE = "Hello, %s!";
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format(TEMPLATE, name);
+    }
 }
