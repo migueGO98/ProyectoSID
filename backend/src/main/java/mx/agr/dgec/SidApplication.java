4d09ebfa87267343056eb0d56759c53767249035
Miguel Guzman <miguel.gm98@gmail.com>
2023-10-17 18:25:05 -0600

    Crear backend

---
+++ backend/src/main/java/mx/agr/dgec/SidApplication.java
@@ -0,0 +1,13 @@
+package mx.agr.dgec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/mx/agr/dgec/controllers/Ejemplo.java
@@ -0,0 +1,4 @@
+package mx.agr.dgec.controllers;
+
+public class Ejemplo {
+}
