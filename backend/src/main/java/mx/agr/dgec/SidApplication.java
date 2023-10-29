792906c7939124b0f62becb73c253396aa0540af
Miguel Guzman <miguel.gm98@gmail.com>
2023-10-28 20:50:00 -0600

    Agregar ignore a carpeta raiz

--- backend/src/main/java/mx/agr/dgec/SidApplication.java
+++ backend/src/main/java/mx/agr/dgec/SidApplication.java
@@ -3,6 +3,16 @@ package mx.agr.dgec;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal de la aplicación.
+ *
+ * @Author Miguel Guzman
+ * @Date 2023-10-17
+ * @implNote Este proyecto solo se usa con fines de aprendizaje. No se debe usar
+ * para fines de producción o comercialización.
+ * @version 1.0.0-SNAPSHOT
+ * @since 1.0.0-SNAPSHOT
+ */
 @SpringBootApplication
 public class SidApplication {
 
