a78413ae1e2aa81c27ed8f1e4dbeb961b5f1cb25
Miguel Guzman <miguel.gm98@gmail.com>
2024-02-21 00:09:55 -0600

    Actualizar codigo autogenerado

---
+++ backend/src/main/java/mx/agr/dgec/generate/model/DomicilioDto.java
@@ -0,0 +1,222 @@
+package mx.agr.dgec.generate.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * Domicilio particular del empleado
+ */
+
+@Schema(name = "Domicilio", description = "Domicilio particular del empleado")
+@JsonTypeName("Domicilio")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+public class DomicilioDto {
+
+  private String calle;
+
+  private String codigoPostal;
+
+  private String colonia;
+
+  private String municipio;
+
+  private String ciudad;
+
+  private String estado;
+
+  public DomicilioDto() {
+    super();
+  }
+
+  /**
+   * Constructor with only required parameters
+   */
+  public DomicilioDto(String calle, String codigoPostal, String colonia, String municipio, String ciudad, String estado) {
+    this.calle = calle;
+    this.codigoPostal = codigoPostal;
+    this.colonia = colonia;
+    this.municipio = municipio;
+    this.ciudad = ciudad;
+    this.estado = estado;
+  }
+
+  public DomicilioDto calle(String calle) {
+    this.calle = calle;
+    return this;
+  }
+
+  /**
+   * Get calle
+   * @return calle
+  */
+  @NotNull 
+  @Schema(name = "calle", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("calle")
+  public String getCalle() {
+    return calle;
+  }
+
+  public void setCalle(String calle) {
+    this.calle = calle;
+  }
+
+  public DomicilioDto codigoPostal(String codigoPostal) {
+    this.codigoPostal = codigoPostal;
+    return this;
+  }
+
+  /**
+   * Get codigoPostal
+   * @return codigoPostal
+  */
+  @NotNull @Size(min = 5, max = 5) 
+  @Schema(name = "codigoPostal", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("codigoPostal")
+  public String getCodigoPostal() {
+    return codigoPostal;
+  }
+
+  public void setCodigoPostal(String codigoPostal) {
+    this.codigoPostal = codigoPostal;
+  }
+
+  public DomicilioDto colonia(String colonia) {
+    this.colonia = colonia;
+    return this;
+  }
+
+  /**
+   * Get colonia
+   * @return colonia
+  */
+  @NotNull 
+  @Schema(name = "colonia", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("colonia")
+  public String getColonia() {
+    return colonia;
+  }
+
+  public void setColonia(String colonia) {
+    this.colonia = colonia;
+  }
+
+  public DomicilioDto municipio(String municipio) {
+    this.municipio = municipio;
+    return this;
+  }
+
+  /**
+   * Get municipio
+   * @return municipio
+  */
+  @NotNull 
+  @Schema(name = "municipio", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("municipio")
+  public String getMunicipio() {
+    return municipio;
+  }
+
+  public void setMunicipio(String municipio) {
+    this.municipio = municipio;
+  }
+
+  public DomicilioDto ciudad(String ciudad) {
+    this.ciudad = ciudad;
+    return this;
+  }
+
+  /**
+   * Get ciudad
+   * @return ciudad
+  */
+  @NotNull 
+  @Schema(name = "ciudad", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("ciudad")
+  public String getCiudad() {
+    return ciudad;
+  }
+
+  public void setCiudad(String ciudad) {
+    this.ciudad = ciudad;
+  }
+
+  public DomicilioDto estado(String estado) {
+    this.estado = estado;
+    return this;
+  }
+
+  /**
+   * Get estado
+   * @return estado
+  */
+  @NotNull 
+  @Schema(name = "estado", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("estado")
+  public String getEstado() {
+    return estado;
+  }
+
+  public void setEstado(String estado) {
+    this.estado = estado;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    DomicilioDto domicilio = (DomicilioDto) o;
+    return Objects.equals(this.calle, domicilio.calle) &&
+        Objects.equals(this.codigoPostal, domicilio.codigoPostal) &&
+        Objects.equals(this.colonia, domicilio.colonia) &&
+        Objects.equals(this.municipio, domicilio.municipio) &&
+        Objects.equals(this.ciudad, domicilio.ciudad) &&
+        Objects.equals(this.estado, domicilio.estado);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(calle, codigoPostal, colonia, municipio, ciudad, estado);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class DomicilioDto {\n");
+    sb.append("    calle: ").append(toIndentedString(calle)).append("\n");
+    sb.append("    codigoPostal: ").append(toIndentedString(codigoPostal)).append("\n");
+    sb.append("    colonia: ").append(toIndentedString(colonia)).append("\n");
+    sb.append("    municipio: ").append(toIndentedString(municipio)).append("\n");
+    sb.append("    ciudad: ").append(toIndentedString(ciudad)).append("\n");
+    sb.append("    estado: ").append(toIndentedString(estado)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ backend/src/main/java/mx/agr/dgec/generate/model/PersonaDto.java
@@ -0,0 +1,453 @@
+package mx.agr.dgec.generate.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.time.LocalDate;
+import mx.agr.dgec.generate.model.EstadoCivilEnumDto;
+import mx.agr.dgec.generate.model.GeneroEnumDto;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * Atributos que tiene una persona
+ */
+
+@Schema(name = "Persona", description = "Atributos que tiene una persona")
+@JsonTypeName("Persona")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+public class PersonaDto {
+
+  private String nombre;
+
+  private String apellidoPaterno;
+
+  private JsonNullable<String> apellidoMaterno = JsonNullable.<String>undefined();
+
+  private String curp;
+
+  private String rfc;
+
+  private String numeroSeguroSocial;
+
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+  private LocalDate fechaNacimiento;
+
+  private Integer edad;
+
+  private GeneroEnumDto genero;
+
+  private String telefonoPersonal;
+
+  private String correoElectronicoPersonal;
+
+  private EstadoCivilEnumDto estadoCivil;
+
+  private Boolean hijos;
+
+  private String contactoEmergenciaNombre;
+
+  private String contactoEmergenciaTelefono;
+
+  public PersonaDto() {
+    super();
+  }
+
+  /**
+   * Constructor with only required parameters
+   */
+  public PersonaDto(String nombre, String apellidoPaterno, String apellidoMaterno, String curp, String rfc, String numeroSeguroSocial, LocalDate fechaNacimiento, Integer edad, GeneroEnumDto genero, String telefonoPersonal, String correoElectronicoPersonal, EstadoCivilEnumDto estadoCivil, Boolean hijos, String contactoEmergenciaNombre, String contactoEmergenciaTelefono) {
+    this.nombre = nombre;
+    this.apellidoPaterno = apellidoPaterno;
+    this.apellidoMaterno = JsonNullable.of(apellidoMaterno);
+    this.curp = curp;
+    this.rfc = rfc;
+    this.numeroSeguroSocial = numeroSeguroSocial;
+    this.fechaNacimiento = fechaNacimiento;
+    this.edad = edad;
+    this.genero = genero;
+    this.telefonoPersonal = telefonoPersonal;
+    this.correoElectronicoPersonal = correoElectronicoPersonal;
+    this.estadoCivil = estadoCivil;
+    this.hijos = hijos;
+    this.contactoEmergenciaNombre = contactoEmergenciaNombre;
+    this.contactoEmergenciaTelefono = contactoEmergenciaTelefono;
+  }
+
+  public PersonaDto nombre(String nombre) {
+    this.nombre = nombre;
+    return this;
+  }
+
+  /**
+   * Get nombre
+   * @return nombre
+  */
+  @NotNull 
+  @Schema(name = "nombre", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("nombre")
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public PersonaDto apellidoPaterno(String apellidoPaterno) {
+    this.apellidoPaterno = apellidoPaterno;
+    return this;
+  }
+
+  /**
+   * Get apellidoPaterno
+   * @return apellidoPaterno
+  */
+  @NotNull 
+  @Schema(name = "apellidoPaterno", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("apellidoPaterno")
+  public String getApellidoPaterno() {
+    return apellidoPaterno;
+  }
+
+  public void setApellidoPaterno(String apellidoPaterno) {
+    this.apellidoPaterno = apellidoPaterno;
+  }
+
+  public PersonaDto apellidoMaterno(String apellidoMaterno) {
+    this.apellidoMaterno = JsonNullable.of(apellidoMaterno);
+    return this;
+  }
+
+  /**
+   * Get apellidoMaterno
+   * @return apellidoMaterno
+  */
+  @NotNull 
+  @Schema(name = "apellidoMaterno", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("apellidoMaterno")
+  public JsonNullable<String> getApellidoMaterno() {
+    return apellidoMaterno;
+  }
+
+  public void setApellidoMaterno(JsonNullable<String> apellidoMaterno) {
+    this.apellidoMaterno = apellidoMaterno;
+  }
+
+  public PersonaDto curp(String curp) {
+    this.curp = curp;
+    return this;
+  }
+
+  /**
+   * Get curp
+   * @return curp
+  */
+  @NotNull @Size(min = 18, max = 18) 
+  @Schema(name = "curp", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("curp")
+  public String getCurp() {
+    return curp;
+  }
+
+  public void setCurp(String curp) {
+    this.curp = curp;
+  }
+
+  public PersonaDto rfc(String rfc) {
+    this.rfc = rfc;
+    return this;
+  }
+
+  /**
+   * Get rfc
+   * @return rfc
+  */
+  @NotNull @Size(min = 13, max = 13) 
+  @Schema(name = "rfc", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("rfc")
+  public String getRfc() {
+    return rfc;
+  }
+
+  public void setRfc(String rfc) {
+    this.rfc = rfc;
+  }
+
+  public PersonaDto numeroSeguroSocial(String numeroSeguroSocial) {
+    this.numeroSeguroSocial = numeroSeguroSocial;
+    return this;
+  }
+
+  /**
+   * Get numeroSeguroSocial
+   * @return numeroSeguroSocial
+  */
+  @NotNull @Size(min = 11, max = 11) 
+  @Schema(name = "numeroSeguroSocial", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("numeroSeguroSocial")
+  public String getNumeroSeguroSocial() {
+    return numeroSeguroSocial;
+  }
+
+  public void setNumeroSeguroSocial(String numeroSeguroSocial) {
+    this.numeroSeguroSocial = numeroSeguroSocial;
+  }
+
+  public PersonaDto fechaNacimiento(LocalDate fechaNacimiento) {
+    this.fechaNacimiento = fechaNacimiento;
+    return this;
+  }
+
+  /**
+   * Get fechaNacimiento
+   * @return fechaNacimiento
+  */
+  @NotNull @Valid 
+  @Schema(name = "fechaNacimiento", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("fechaNacimiento")
+  public LocalDate getFechaNacimiento() {
+    return fechaNacimiento;
+  }
+
+  public void setFechaNacimiento(LocalDate fechaNacimiento) {
+    this.fechaNacimiento = fechaNacimiento;
+  }
+
+  public PersonaDto edad(Integer edad) {
+    this.edad = edad;
+    return this;
+  }
+
+  /**
+   * Get edad
+   * @return edad
+  */
+  @NotNull 
+  @Schema(name = "edad", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("edad")
+  public Integer getEdad() {
+    return edad;
+  }
+
+  public void setEdad(Integer edad) {
+    this.edad = edad;
+  }
+
+  public PersonaDto genero(GeneroEnumDto genero) {
+    this.genero = genero;
+    return this;
+  }
+
+  /**
+   * Get genero
+   * @return genero
+  */
+  @NotNull @Valid 
+  @Schema(name = "genero", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("genero")
+  public GeneroEnumDto getGenero() {
+    return genero;
+  }
+
+  public void setGenero(GeneroEnumDto genero) {
+    this.genero = genero;
+  }
+
+  public PersonaDto telefonoPersonal(String telefonoPersonal) {
+    this.telefonoPersonal = telefonoPersonal;
+    return this;
+  }
+
+  /**
+   * Get telefonoPersonal
+   * @return telefonoPersonal
+  */
+  @NotNull @Size(min = 10, max = 10) 
+  @Schema(name = "telefonoPersonal", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("telefonoPersonal")
+  public String getTelefonoPersonal() {
+    return telefonoPersonal;
+  }
+
+  public void setTelefonoPersonal(String telefonoPersonal) {
+    this.telefonoPersonal = telefonoPersonal;
+  }
+
+  public PersonaDto correoElectronicoPersonal(String correoElectronicoPersonal) {
+    this.correoElectronicoPersonal = correoElectronicoPersonal;
+    return this;
+  }
+
+  /**
+   * Get correoElectronicoPersonal
+   * @return correoElectronicoPersonal
+  */
+  @NotNull @jakarta.validation.constraints.Email
+  @Schema(name = "correoElectronicoPersonal", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("correoElectronicoPersonal")
+  public String getCorreoElectronicoPersonal() {
+    return correoElectronicoPersonal;
+  }
+
+  public void setCorreoElectronicoPersonal(String correoElectronicoPersonal) {
+    this.correoElectronicoPersonal = correoElectronicoPersonal;
+  }
+
+  public PersonaDto estadoCivil(EstadoCivilEnumDto estadoCivil) {
+    this.estadoCivil = estadoCivil;
+    return this;
+  }
+
+  /**
+   * Get estadoCivil
+   * @return estadoCivil
+  */
+  @NotNull @Valid 
+  @Schema(name = "estadoCivil", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("estadoCivil")
+  public EstadoCivilEnumDto getEstadoCivil() {
+    return estadoCivil;
+  }
+
+  public void setEstadoCivil(EstadoCivilEnumDto estadoCivil) {
+    this.estadoCivil = estadoCivil;
+  }
+
+  public PersonaDto hijos(Boolean hijos) {
+    this.hijos = hijos;
+    return this;
+  }
+
+  /**
+   * Get hijos
+   * @return hijos
+  */
+  @NotNull 
+  @Schema(name = "hijos", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("hijos")
+  public Boolean getHijos() {
+    return hijos;
+  }
+
+  public void setHijos(Boolean hijos) {
+    this.hijos = hijos;
+  }
+
+  public PersonaDto contactoEmergenciaNombre(String contactoEmergenciaNombre) {
+    this.contactoEmergenciaNombre = contactoEmergenciaNombre;
+    return this;
+  }
+
+  /**
+   * Get contactoEmergenciaNombre
+   * @return contactoEmergenciaNombre
+  */
+  @NotNull 
+  @Schema(name = "contactoEmergenciaNombre", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("contactoEmergenciaNombre")
+  public String getContactoEmergenciaNombre() {
+    return contactoEmergenciaNombre;
+  }
+
+  public void setContactoEmergenciaNombre(String contactoEmergenciaNombre) {
+    this.contactoEmergenciaNombre = contactoEmergenciaNombre;
+  }
+
+  public PersonaDto contactoEmergenciaTelefono(String contactoEmergenciaTelefono) {
+    this.contactoEmergenciaTelefono = contactoEmergenciaTelefono;
+    return this;
+  }
+
+  /**
+   * Get contactoEmergenciaTelefono
+   * @return contactoEmergenciaTelefono
+  */
+  @NotNull @Size(min = 10, max = 10) 
+  @Schema(name = "contactoEmergenciaTelefono", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("contactoEmergenciaTelefono")
+  public String getContactoEmergenciaTelefono() {
+    return contactoEmergenciaTelefono;
+  }
+
+  public void setContactoEmergenciaTelefono(String contactoEmergenciaTelefono) {
+    this.contactoEmergenciaTelefono = contactoEmergenciaTelefono;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PersonaDto persona = (PersonaDto) o;
+    return Objects.equals(this.nombre, persona.nombre) &&
+        Objects.equals(this.apellidoPaterno, persona.apellidoPaterno) &&
+        Objects.equals(this.apellidoMaterno, persona.apellidoMaterno) &&
+        Objects.equals(this.curp, persona.curp) &&
+        Objects.equals(this.rfc, persona.rfc) &&
+        Objects.equals(this.numeroSeguroSocial, persona.numeroSeguroSocial) &&
+        Objects.equals(this.fechaNacimiento, persona.fechaNacimiento) &&
+        Objects.equals(this.edad, persona.edad) &&
+        Objects.equals(this.genero, persona.genero) &&
+        Objects.equals(this.telefonoPersonal, persona.telefonoPersonal) &&
+        Objects.equals(this.correoElectronicoPersonal, persona.correoElectronicoPersonal) &&
+        Objects.equals(this.estadoCivil, persona.estadoCivil) &&
+        Objects.equals(this.hijos, persona.hijos) &&
+        Objects.equals(this.contactoEmergenciaNombre, persona.contactoEmergenciaNombre) &&
+        Objects.equals(this.contactoEmergenciaTelefono, persona.contactoEmergenciaTelefono);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(nombre, apellidoPaterno, apellidoMaterno, curp, rfc, numeroSeguroSocial, fechaNacimiento, edad, genero, telefonoPersonal, correoElectronicoPersonal, estadoCivil, hijos, contactoEmergenciaNombre, contactoEmergenciaTelefono);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PersonaDto {\n");
+    sb.append("    nombre: ").append(toIndentedString(nombre)).append("\n");
+    sb.append("    apellidoPaterno: ").append(toIndentedString(apellidoPaterno)).append("\n");
+    sb.append("    apellidoMaterno: ").append(toIndentedString(apellidoMaterno)).append("\n");
+    sb.append("    curp: ").append(toIndentedString(curp)).append("\n");
+    sb.append("    rfc: ").append(toIndentedString(rfc)).append("\n");
+    sb.append("    numeroSeguroSocial: ").append(toIndentedString(numeroSeguroSocial)).append("\n");
+    sb.append("    fechaNacimiento: ").append(toIndentedString(fechaNacimiento)).append("\n");
+    sb.append("    edad: ").append(toIndentedString(edad)).append("\n");
+    sb.append("    genero: ").append(toIndentedString(genero)).append("\n");
+    sb.append("    telefonoPersonal: ").append(toIndentedString(telefonoPersonal)).append("\n");
+    sb.append("    correoElectronicoPersonal: ").append(toIndentedString(correoElectronicoPersonal)).append("\n");
+    sb.append("    estadoCivil: ").append(toIndentedString(estadoCivil)).append("\n");
+    sb.append("    hijos: ").append(toIndentedString(hijos)).append("\n");
+    sb.append("    contactoEmergenciaNombre: ").append(toIndentedString(contactoEmergenciaNombre)).append("\n");
+    sb.append("    contactoEmergenciaTelefono: ").append(toIndentedString(contactoEmergenciaTelefono)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
