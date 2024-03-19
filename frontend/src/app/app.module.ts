6e4dfd8e552555f6982475406e75bc71e5535de7
Miguel Guzman <miguel.gm98@gmail.com>
2024-03-18 23:09:54 -0600

    Crear Estructura

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './features/home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule],
+  imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
