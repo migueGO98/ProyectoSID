e6f66148af1b79d693dbf9006496e8b0d3923dbb
Miguel Guzman <miguel.guzman@pagofacil.net>
2024-05-20 20:22:53 -0600

    Agregar anotacion Repository a RepositorioRol

--- backend/src/main/java/mx/agr/dgec/repositorios/RepositorioRol.java
+++ backend/src/main/java/mx/agr/dgec/repositorios/RepositorioRol.java
@@ -2,6 +2,8 @@ package mx.agr.dgec.repositorios;
 
 import mx.agr.dgec.entidades.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositorioRol extends JpaRepository<Rol, String> {
 }
