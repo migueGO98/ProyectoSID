1b9c21f42de0ed974c696006ac9a46a993ab15f3
Miguel Guzman <miguel.guzman@pagofacil.net>
2024-05-21 13:35:31 -0600

    Corregir pruebas unitarias enum estado civil

--- backend/src/test/java/mx/agr/dgec/SID/enums/EstadoCivilEnumTest.java
+++ backend/src/test/java/mx/agr/dgec/SID/enums/EstadoCivilEnumTest.java
@@ -6,7 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 class EstadoCivilEnumTest {
@@ -18,13 +22,13 @@ class EstadoCivilEnumTest {
     @Test
     void validarSizeEstadosCivilesEnum() {
         // Arrange
-        var estadosCivilesSizeEsperado = EstadoCivilEnum.values().length;
+        var estadosCivilesEsperado = EstadoCivilEnum.values();
 
         // Act
-        var estadosCivilesDto = EstadoCivilEnumDto.values().length;
+        var estadosCivilesDto = EstadoCivilEnumDto.values();
 
         // Assert
-        assertEquals(estadosCivilesSizeEsperado, estadosCivilesDto, "El tamaño de los enums de los Estados Civiles no es el mismo");
+        assertEquals(estadosCivilesEsperado.length, estadosCivilesDto.length, "El tamaño de los enums de los Estados Civiles no es el mismo");
     }
 
     /**
@@ -34,14 +38,18 @@ class EstadoCivilEnumTest {
     @Test
     void validarValoresEstadosCivilesEnum() {
         // Arrange
-        var estadosCivilesEsperados = EstadoCivilEnum.values();
-        var estadosCivilesDto = EstadoCivilEnumDto.values();
+        var estadosCivilesEsperados = Arrays.stream(EstadoCivilEnum.values()).map(EstadoCivilEnum::name).collect(Collectors.toSet());
+        var estadosCivilesDto = Arrays.stream(EstadoCivilEnumDto.values()).map(EstadoCivilEnumDto::name).collect(Collectors.toSet());
 
         // Act with stream
-        for( int i = 0; i < estadosCivilesDto.length; i++ ) {
-            // Assert
-            assertEquals(estadosCivilesEsperados[i].name(), estadosCivilesDto[i].name(), "EstadosCivilesEnumDto no tiene los mismos valores que EstadosCivilesEnum");
-        }
+            // Validar que los valores de EstadosCivilesEnumDto estén en EstadosCivilesEnum
+        boolean estadosCivilesDtoInEstadosCiviles = estadosCivilesEsperados.containsAll(estadosCivilesDto);
+            // Validar que los valores de EstadosCivilesEnum estén en EstadosCivilesEnumDto
+        boolean estadosCivilesInEstadosCivilesDto = estadosCivilesDto.containsAll(estadosCivilesEsperados);
+
+        // Assert
+        assertTrue(estadosCivilesDtoInEstadosCiviles, "Hay valores en EstadosCivilesEnumDto que no están en EstadosCivilesEnum");
+        assertTrue(estadosCivilesInEstadosCivilesDto, "Hay valores en EstadosCivilesEnum que no están en EstadosCivilesEnumDto");
     }
 
 }
