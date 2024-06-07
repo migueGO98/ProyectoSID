57375c50d057ff872c5660d7b737be9876bf6cf8
Miguel Guzman <miguel.guzman@pagofacil.net>
2024-06-07 14:03:30 -0600

    Configurar AzureGraph para usar su API y modificar SecurityWeb para poder ingresar a la consola de la BD H2

---
+++ backend/src/main/java/mx/agr/dgec/config/AzureGraph.java
@@ -0,0 +1,29 @@
+package mx.agr.dgec.config;
+
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.microsoft.graph.serviceclient.GraphServiceClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AzureGraph {
+    @Value("${azure.ad.b2c.client-id}")
+    private String clientId;
+
+    @Value("${azure.ad.b2c.client-secret}")
+    private String clientSecret;
+
+    @Value("${azure.ad.b2c.tenant}")
+    private String tenantId;
+
+    @Bean
+    public GraphServiceClient graphClient() {
+        var credential = new ClientSecretCredentialBuilder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .tenantId(tenantId)
+                .build();
+        return new GraphServiceClient(credential);
+    }
+}
