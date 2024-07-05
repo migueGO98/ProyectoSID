c3d23464e24d5ba452b53770b7be008fe9bdcd5b
Miguel Guzman <miguel.guzman@pagofacil.net>
2024-07-05 11:35:42 -0600

    Actualizar API en frontend

--- frontend/src/app/generate/openapi/configuration.ts
+++ frontend/src/app/generate/openapi/configuration.ts
@@ -86,6 +86,15 @@ export class Configuration {
         else {
             this.credentials = {};
         }
+
+        // init default jwt credential
+        if (!this.credentials['jwt']) {
+            this.credentials['jwt'] = () => {
+                return typeof this.accessToken === 'function'
+                    ? this.accessToken()
+                    : this.accessToken;
+            };
+        }
     }
 
     /**
