cb20f312165dc6428411a22f7f34fd0698e19335
Miguel Guzman <miguel.guzman@pagofacil.net>
2024-07-31 10:15:37 -0600

    Agregar configuracion a OpenAPI(SpringDoc) para no mostrar Content en respuestas 204

--- backend/src/main/java/mx/agr/dgec/config/SpringDoc.java
+++ backend/src/main/java/mx/agr/dgec/config/SpringDoc.java
@@ -8,6 +8,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -39,4 +44,21 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer",
         bearerFormat = "JWT"
 )
-public class SpringDoc {}
+public class SpringDoc {
+
+    /*
+     * Personalizar OpenApi-Spring Doc para que no muestre algun contenido en las respuestas 204
+     */
+    @Bean
+    public OpenApiCustomizer customGlobalHeaderOpenApiCustomizer() {
+        return openApi ->
+            openApi.getPaths().forEach((path, pathItem) -> pathItem.readOperations().forEach(operation -> {
+                ApiResponses apiResponses = operation.getResponses();
+                ApiResponse noContentResponse = apiResponses.get("204");
+                if (noContentResponse != null) {
+                    noContentResponse.setContent(new Content());
+                }
+            }));
+
+    }
+}
